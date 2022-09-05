277f51cd98784657e3f4afeccb8064f8920b3692
aida radfard <aida.radfard.1@gmail.com>
2022-09-05 18:19:42 +0430

    card box style  and background images

---
+++ src/views/CardLeft.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../statics/sass/card.scss';
+import '../statics/sass/left-card.scss';
+
+export const CardLeft = () => {
+  return (
+    <div className='card'>
+        <div className='left-card'>
+
+        </div>
+    </div>
+  )
+}
---
+++ src/views/CardRight.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../statics/sass/right-card.scss';
+
+
+export const CardRight = () => {
+  return (
+    <div className='card'>
+        <div className='right-card'>
+          <div className='right-card-cover'>
+
+          </div>
+        </div>
+    </div>
+  )
+}
